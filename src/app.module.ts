ee24725231a69b3760ef7c6c62de2c06c8a93655
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2024-03-07 20:35:11 -0300

    feat: jwt token authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { AuthModule } from './auth/auth.modules'
+import { CreateQuestionController } from './controllers/create-question.controller'
 
 @Module({
   imports: [
@@ -10,9 +13,13 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
-
